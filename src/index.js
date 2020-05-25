19b7616bb9f367baa84b12c5c447279924274e45
Joalbert <joalbertgonzalez@gmail.com>
2020-05-25 16:28:34 -0500

    [ADD]🚀 provider and store added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import './index.css';
+import 'font-awesome/css/font-awesome.css';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
-import 'font-awesome/css/font-awesome.css'
+import generateStore from './redux/store';
+
+const store = generateStore();
 
 let WithRouter = () => <BrowserRouter><App /></BrowserRouter>
+const WithStore = () => <Provider store={store} ><WithRouter /></Provider>
 
-ReactDOM.render(<WithRouter />, document.getElementById('root'));
+ReactDOM.render(<WithStore />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
