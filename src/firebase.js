dbb6b46f5d14095b2e3ae0126783e21f10d1265c
Joalbert <joalbertgonzalez@gmail.com>
2020-05-25 20:13:31 -0500

    [ADD]🚀 firebase credentials added

---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAS28uwo_yfReZ90q4KSWXQtn86q3-bzVU",
+  authDomain: "characters-bc09b.firebaseapp.com",
+  databaseURL: "https://characters-bc09b.firebaseio.com",
+  projectId: "characters-bc09b",
+  storageBucket: "characters-bc09b.appspot.com",
+  messagingSenderId: "1025069463016",
+  appId: "1:1025069463016:web:0c092129aaee0fa982e05d",
+  measurementId: "G-SXPYHWELWF"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const loginWithGoogle() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  return firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(snap => snap.user)
+    .catch(console.log);
+}
+
