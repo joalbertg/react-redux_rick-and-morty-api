f81a9f881b84c6ecfa20d8111e60407c15228496
Joalbert <joalbertgonzalez@gmail.com>
2020-05-25 19:18:52 -0500

    [ADD]🚀 connect and mapStateToProps in Home component added

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,46 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import Card from '../card/Card'
-import styles from './home.module.css'
-import axios from 'axios'
+import React from 'react';
+import { connect } from 'react-redux';
 
-let URL = "https://rickandmortyapi.com/api"
+import Card from '../card/Card';
+import styles from './home.module.css';
 
-export default function Home() {
-
-    let [chars, setChars] = useState([])
-
-    useEffect(() => {
-        getCharacters()
-    }, [])
-
-    function nextChar() {
-        chars.shift()
-        if (!chars.length) {
-            //get more characters
-        }
-        setChars([...chars])
-    }
-
-    function renderCharacter() {
-        let char = chars[0]
-        return (
-            <Card leftClick={nextChar} {...char} />
-        )
-    }
-
-    function getCharacters() {
-        return axios.get(`${URL}/character`)
-            .then(res => {
-                setChars(res.data.results)
-            })
+function Home({ chars }) {
+    const renderCharacter = () => {
+      let char = chars[0]
+      return <Card {...char} />
     }
 
     return (
-        <div className={styles.container}>
-            <h2>Personajes de Rick y Morty</h2>
-            <div>
-                {renderCharacter()}
-            </div>
+      <div className={styles.container}>
+        <h2>Personajes de Rick y Morty</h2>
+        <div>
+          { renderCharacter() }
         </div>
+      </div>
     )
 }
+
+const mapStateToProps = state => {
+  return {
+    chars: state.characters.array
+  }
+};
+
+export default connect(mapStateToProps)(Home);
+
