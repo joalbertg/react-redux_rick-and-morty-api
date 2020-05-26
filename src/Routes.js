d5d1e5ff2910d6a7481a5ee3996590a7feeccb02
Joalbert <joalbertgonzalez@gmail.com>
2020-05-26 00:54:10 -0500

    [ADD]🚀 redirect with redux added

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,30 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Home from './components/home/HomePage'
-import FavPage from './components/favs/FavPage'
-import LoginPage from './components/login/LoginPage'
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default function Routes() {
-    return (
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/favs" component={FavPage} />
-            <Route path="/login" component={LoginPage} />
-        </Switch>
-    )
+import Home from './components/home/HomePage';
+import FavPage from './components/favs/FavPage';
+import LoginPage from './components/login/LoginPage';
+
+const PrivateRoute = ({ path, component, loggedIn, ...rest}) => {
+  if (loggedIn) {
+    return <Route path={path} component={component} {...rest} />
+  } else {
+    return <Redirect to='/login' {...rest} />
+  }
 }
+
+const Routes = ({loggedIn}) => (
+  <Switch>
+    <PrivateRoute exact path="/" loggedIn={loggedIn} component={Home} />
+    <PrivateRoute path="/favs" loggedIn={loggedIn} component={FavPage} />
+    <Route path="/login" component={LoginPage} />
+  </Switch>
+);
+
+const mapStateToProps = ({user: {loggedIn}}) => ({
+  loggedIn
+});
+
+export default connect(mapStateToProps)(Routes);
+
